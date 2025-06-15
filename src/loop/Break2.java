d6a3a1541c07ec441e9e36d2093e46a7c18f843e
luna <kkangthfud@gmail.com>
2025-06-15 19:44:29 +0900

    두 번째 커밋

--- src/loop/Break2.java
+++ src/loop/Break2.java
@@ -10,7 +10,7 @@ public class Break2 {
             if (sum > 10) {
                 System.out.println("합이 10보다 크면 종료: i=" + i + ", sum=" + sum);
                 break;
-                //test
+
             }
             i++;
         }
